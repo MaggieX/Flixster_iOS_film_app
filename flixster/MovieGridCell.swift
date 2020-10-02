f45594836ed8830e371ea5c69f712c95fcb76cb8
MaggieX <zxiao2015@gmail.com>
2020-10-02 15:33:27 -0700

    Flixster Part 2 done. Added MovieGridCell, MovieGridViewController

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Maggie Xiao on 09/28/2020.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
